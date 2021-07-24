a62ceef5fc69f29fa431ff9a4da3e1518e9c0255
Dániel Szabó <25702868+szabodanika@users.noreply.github.com>
2021-07-24 15:55:22 +0100

    Update quicksetup.sh

--- quicksetup.sh
+++ quicksetup.sh
@@ -3,5 +3,4 @@ cd /var/www/wake-pc
 wget https://raw.githubusercontent.com/szabodanika/wake-pc/master/index.php
 wget https://raw.githubusercontent.com/szabodanika/wake-pc/master/config-template.ini
 cp config-template.ini config.ini
-sudo vim config.ini
 php -S 0.0.0.0:9010
