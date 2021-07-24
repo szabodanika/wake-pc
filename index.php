14d7292a4574040dfe9b7cef4672e06d4cd90a70
szabodanika <daniel.szabo99@outlook.com>
2021-07-24 19:47:33 +0100

    Added new readme header, added clock emoji to page title

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (($config = parse_ini_file("config.ini", true)) == false) {
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
-	<title>Wake PC</title>
+	<title>⏰ Wake PC</title>
 </head>
 
 <body>
